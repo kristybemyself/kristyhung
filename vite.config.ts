95a36426cea1269ec4544551b496c3f016f38bfc
kristybemyself <yokanikky@gmail.com>
2026-07-14 19:05:55 +0800

    vs code 4

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/kristyhung/", //
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
